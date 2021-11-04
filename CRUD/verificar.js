a451616f69fc5c9c79e0465739f6d6a82cca59f9
RUl <radscjggdiosteama@gmail.com>
2021-11-04 19:40:32 -0400

    CODIGO COMPLETE

--- CRUD/verificar.js
+++ CRUD/verificar.js
@@ -4,13 +4,11 @@ const User = require('../modelos/user')
  async function verificarUser(req,res){
     let acceso = false
         const resultado= await Promise.all([User.find({userName:req.params.user}),User.find({contrasena:req.params.clave})])
-        console.log(resultado[0].length, resultado[1].length)
         if( resultado[0].length == 1 && resultado[1].length == 1 ){
             acceso = true;
         }else{
             acceso = false;
         }
-    console.log(acceso)
     return acceso;
 };
 
