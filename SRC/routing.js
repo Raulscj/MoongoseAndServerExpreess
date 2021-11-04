8671aa95404fe314f2fdf854f73c867283118448
RUl <radscjggdiosteama@gmail.com>
2021-11-04 19:37:15 -0400

    FUNCIONAL 99.99%

--- SRC/routing.js
+++ SRC/routing.js
@@ -7,7 +7,6 @@ const {actualizar, actualizarUser} = require('../CRUD/update');
 const verificar = require('../CRUD/verificar')
 
 router.get('/Producto/params/:user/:clave/:atributo/:dato', (req,res)=>{
-	
 		verificar(req,res)
 		.then(acceso=>{
 			if(acceso){
@@ -19,31 +18,59 @@ router.get('/Producto/params/:user/:clave/:atributo/:dato', (req,res)=>{
 			}
 		})
 	})
-	
-router.post('/Producto', (req,res)=>{
-	console.log(req.body)
-	create(res,req)
-	.then(producto_guardado => res.send(producto_guardado)) 
-	.catch(err => console.log(err))
+
+router.post('/Producto/params/:user/:clave', (req,res)=>{
+	verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			console.log(req.body)
+			create(res,req)
+			.then(producto_guardado => res.send(producto_guardado)) 
+			.catch(err => console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
+	})
 });
 
-router.delete('/Producto/params/:id', (req,res)=>{
-    deleteProducto(req.params.id)
-	.then( _ => res.send('Producto eliminado de la base de datos, satisfactoriamente.')) 
-	.catch(err => console.log(err))
+router.delete('/Producto/params/:user/:clave/:id', (req,res)=>{
+    verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			deleteProducto(req.params.id)
+			.then( _ => res.send('Producto eliminado de la base de datos, satisfactoriamente.')) 
+			.catch(err => console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
+	})
 })
 
-router.put('/Producto/params/:id',(req,res)=>{
-	actualizar(req,res)
-	.then(producto => res.send(producto))
-	.catch(err =>console.log(err))
+router.put('/Producto/params/:user/:clave/:id',(req,res)=>{
+	verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			actualizar(req,res)
+			.then(producto => res.send(producto))
+			.catch(err =>console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
+	})
 });
 //Usuario
-router.get('/Usuario/params/:atributo/:dato', (req,res)=>{
-	buscarUser(req,res)
-	.then(usuario => res.send('El usuario buscado se encuentran en la consola.')) 
-	.catch(err => console.log(err))
+router.get('/Usuario/params/:user/:clave/:atributo/:dato', (req,res)=>{
+	verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			buscarUser(req,res)
+			.then(usuario => res.send('El usuario buscado se encuentran en la consola.')) 
+			.catch(err => console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
 	})
+})
 	
 router.post('/Usuario', (req,res)=>{
 	console.log(req.body)
@@ -52,15 +79,29 @@ router.post('/Usuario', (req,res)=>{
 	.catch(err => console.log(err))
 });
 
-router.delete('/Usuario/params/:id', (req,res)=>{
-    deleteUser(req.params.id)
-	.then( _ => res.send('Usuario eliminado de la base de datos, satisfactoriamente.')) 
-	.catch(err => console.log(err))
+router.delete('/Usuario/params/:user/:clave/:id', (req,res)=>{
+    verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			deleteUser(req.params.id)
+			.then( _ => res.send('Usuario eliminado de la base de datos, satisfactoriamente.')) 
+			.catch(err => console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
+	})
 })
 
-router.put('/Usuario/params/:id',(req,res)=>{
-	actualizarUser(req,res)
-	.then(usuario => res.send(usuario))
-	.catch(err =>console.log(err))
+router.put('/Usuario/params/:user/:clave/:id',(req,res)=>{
+	verificar(req,res)
+	.then(acceso=>{
+		if(acceso){
+			actualizarUser(req,res)
+			.then(usuario => res.send(usuario))
+			.catch(err =>console.log(err))
+		}else{
+			res.send('Usuario Invalido');
+		}
+	})
 });
 module.exports= router;
